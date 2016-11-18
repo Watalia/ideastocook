2ee67f01608f1ab944a873e34bba1aa47af2c5a3
Raghu Betina <raghu@firstdraft.com>
2016-11-18 20:36:54 +0000

    Generate recipes resource with starter:resource

---
+++ db/migrate/20161118203654_create_recipes.rb
@@ -0,0 +1,16 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.integer :cooking_time
+      t.string :name
+      t.string :instructions
+      t.string :final_tips
+      t.string :difficulty
+      t.boolean :favorite
+      t.integer :category_id
+
+      t.timestamps
+
+    end
+  end
+end
